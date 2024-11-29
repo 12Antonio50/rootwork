e2da6e72e876c34bd9c1919e626f33a3854f58f0
12Antonio50 <ja.mojica.a@edu.utc.mx>
2024-11-29 16:55:16 -0600

    image

--- js/script.js
+++ js/script.js
@@ -33,30 +33,37 @@ fetch('footer.html')
     
 //Carousel
 document.addEventListener('DOMContentLoaded', function () {
-    const totalImages = 9; // Cantidad total de imágenes
+    const images = [
+        "/picture/img_1.jpg",
+        "/picture/img_2.jpg",
+        "/picture/img_3.jpg",
+        "/picture/img_4.jpeg",
+        "/picture/img_5.jpg",
+        "/picture/img_6.jpg",
+        "/picture/img_7.jpg",
+        "/picture/img_8.jpg",
+        "/picture/img_9.jpeg"
+    ];
+
     let currentIndex = 0;
+    let totalImages = images.length;
 
     function changeImage() {
-        // Seleccionar la imagen actual y la siguiente
         const currentImageContainer = document.getElementById(`imageContainer${currentIndex + 1}`);
-        const nextIndex = (currentIndex + 1) % totalImages; // Calcular el índice de la siguiente imagen
+        const nextIndex = (currentIndex + 1) % totalImages;
         const nextImageContainer = document.getElementById(`imageContainer${nextIndex + 1}`);
 
-        // Ocultar la imagen actual
+        const imgElement = nextImageContainer.querySelector('img');
+        imgElement.src = images[nextIndex];
+
         currentImageContainer.classList.remove('show', 'zoom');
         currentImageContainer.classList.add('hide');
 
-        // Mostrar la siguiente imagen
         nextImageContainer.classList.remove('hide');
         nextImageContainer.classList.add('show', 'zoom');
-
-        // Actualizar el índice actual
         currentIndex = nextIndex;
     }
 
-    // Cambiar de imagen cada 5 segundos
     setInterval(changeImage, 5000);
-
-    // Mostrar la primera imagen inicialmente
     changeImage();
 });
