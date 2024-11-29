e25aff23eb09dcf434aad46e34a57033c6d36889
12Antonio50 <ja.mojica.a@edu.utc.mx>
2024-11-29 10:21:40 -0600

    Commit

--- script.js
+++ script.js
@@ -33,15 +33,15 @@ fetch('footer.html')
 //Carousel
 document.addEventListener('DOMContentLoaded', function () {
     const images = [
-        "../picture/img_1.jpg",
-        "../picture/img_2.jpg",
-        "../picture/img_3.jpg",
-        "../picture/img_4.jpeg",
-        "../picture/img_5.jpg",
-        "../picture/img_6.jpg",
-        "../picture/img_7.jpg",
-        "../picture/img_8.jpg",
-        "../picture/img_9.jpeg"
+        "./picture/img_1.jpg",
+        "./picture/img_2.jpg",
+        "./picture/img_3.jpg",
+        "./picture/img_4.jpeg",
+        "./picture/img_5.jpg",
+        "./picture/img_6.jpg",
+        "./picture/img_7.jpg",
+        "./picture/img_8.jpg",
+        "./picture/img_9.jpeg"
     ];
 
     let currentIndex = 0;
